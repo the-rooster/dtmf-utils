2aefe9e17030f58534c548353ec69a8840f48dd8
AnnoyingCotton <andrewmjculberson@gmail.com>
2021-08-11 00:10:51 -0600

    bug fixin
    decoding is fixed i am pretty sure!

--- src/decode.py
+++ src/decode.py
@@ -39,10 +39,11 @@ def decode_string(string):
                 
                 #print("THINGS:",currNum[i+1:],currS + dicts.num_to_letter[temp[0]][len(temp) - 1])
                 #print("THINGS 2:",len(currNum))
-                if len(currNum) > 1:
-                    ret = self.helper(currNum[i+1:],currS + dicts.num_to_letter[temp[0]][len(temp) - 1])
-                else:
-                    ret = self.helper("",currS + dicts.num_to_letter[temp[0]][len(temp) - 1])
+                if len(temp) - 1 < len(dicts.num_to_letter[temp[0]]):
+                    if len(currNum) > 1:
+                        ret = self.helper(currNum[i+1:],currS + dicts.num_to_letter[temp[0]][len(temp) - 1])
+                    else:
+                        ret = self.helper("",currS + dicts.num_to_letter[temp[0]][len(temp) - 1])
                 
                 if ret != None:
                     self.res.append(ret)
@@ -64,47 +65,47 @@ def decode_audio(fname):
 
     sample_rate, samples = wavfile.read(fname)
     #print(sample_rate,len(samples))
-    f,t,Zxx = spectrogram(samples,sample_rate,nperseg=512,noverlap=256)
+    f,t,Zxx = spectrogram(samples,sample_rate,nperseg=512,noverlap=511)
 
     cv2.imshow("test",cv2.resize(Zxx / np.max(Zxx),(1000,1000)))
     cv2.waitKey()
 
+    Zxx_scaled = Zxx / np.max(Zxx)
 
     #iterate through columns of spectrogram and find the relevant frequencies
     
     #bottom frequency max is 941 Hz. break frequencies apart based on this boundary
 
-    freq_thresh_bot = 941
+    freq_thresh_bot = 942
 
-    freq_thresh_top = 1209
+    freq_thresh_top = 1208
 
     num_res = ""
 
     in_signal = False
-
-    for i in range(Zxx.T.shape[0]):
-        col = Zxx.T[i]
+    freqs = []
+    for i in range(Zxx_scaled.T.shape[0]):
+        col = Zxx_scaled.T[i]
 
         #threshold frequencies
 
-        freqs = []
 
-        if in_signal:
-            freqs = f[np.where(col > 25000)]
-        else:
-            freqs = f[np.where(col > 55000)]
-
-
+        # if in_signal:
+        #     freqs = f[np.where(col > 0.8)]
+        # else:
+        #     freqs = f[np.where(col > 0.1)]
+        freqs = f[np.where(col > 0.01)]
+        #print(freqs)
 
         if freqs.shape[0] > 0 and not in_signal:
-            
+            print(freqs)
             botFreq = np.mean(freqs[np.where(freqs < freq_thresh_bot)])
 
             topFreq = np.mean(freqs[np.where(freqs > freq_thresh_top)])
 
             
             #print(botFreq,topFreq)
-            mostLikely = ("",1000)
+            mostLikely = ("",100000)
 
             for c,pair in dicts.num_to_freq.items():
                 diff = (pair[0] - botFreq)**2 + (pair[1] - topFreq)**2
@@ -112,11 +113,13 @@ def decode_audio(fname):
                 if diff < mostLikely[1]:
                     mostLikely = (c,diff)
             
+            print("LIKELY:",mostLikely,botFreq,topFreq,freqs)
             num_res += mostLikely[0]
 
             in_signal = True
 
-        else:
+        elif  freqs.shape[0] == 0 and in_signal:
+            print("TEST")
             in_signal = False
 
 
--- src/encode.py
+++ src/encode.py
@@ -71,10 +71,10 @@ def encode_string(frate,nframes,string,sep):
 
 if __name__ == "__main__":
     frate = 4100
-    secsPerTone = 0.5
-    sepSecs = 0.25
+    secsPerTone = 0.52
+    sepSecs = 0.75
     amp = 20000
 
-    waves = encode_string(frate,int(frate*secsPerTone),"andrew",int(frate*sepSecs))
+    waves = encode_string(frate,int(frate*secsPerTone),"andrew made this program woohoo",int(frate*sepSecs))
 
     gen_wav("test.wav",waves,frate,amp,len(waves))
